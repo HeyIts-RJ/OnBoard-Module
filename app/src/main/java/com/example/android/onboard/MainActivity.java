7cd124bed0046cb1674415dddf742b9dd13b15bc
HeyIts-RJ <rishab1300@gmail.com>
2019-07-18 03:07:13 +0530

    Final Completed with working UI and function.

--- app/src/main/java/com/example/android/onboard/MainActivity.java
+++ app/src/main/java/com/example/android/onboard/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.android.onboard;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager mSlideViewPager;
     private LinearLayout mDotLayout;
+
+    private TextView[] mDots;
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+
+        }
+
+        @Override
+        public void onPageSelected(int i) {
+            addDotsIndicators(i);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int i) {
+
+        }
+    };
+    private SliderAdapter sliderAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +38,29 @@ public class MainActivity extends AppCompatActivity {
 
         mSlideViewPager=(ViewPager)findViewById(R.id.slideViewPager);
         mDotLayout=(LinearLayout)findViewById(R.id.dots);
+
+        sliderAdapter = new SliderAdapter(this);
+        mSlideViewPager.setAdapter(sliderAdapter);
+        addDotsIndicators(0);
+
+        mSlideViewPager.addOnPageChangeListener(viewListener);
     }
+
+    public void addDotsIndicators(int position) {
+        mDots = new TextView[3];
+        mDotLayout.removeAllViews();
+
+        for (int i = 0; i < mDots.length; i++) {
+            mDots[i] = new TextView(this);
+            mDots[i].setText(Html.fromHtml("<div style=\"padding-left:50px;padding-right:50px;\">&#8226;</div>"));
+            mDots[i].setTextSize(35);
+            mDots[i].setTextColor(getResources().getColor(R.color.colorTransparentWhite));
+
+            mDotLayout.addView(mDots[i]);
+        }
+        if (mDots.length > 0) {
+            mDots[position].setTextColor(getResources().getColor(R.color.colorAccent));
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/android/onboard/SliderAdapter.java
@@ -0,0 +1,72 @@
+package com.example.android.onboard;
+
+import android.content.Context;
+import android.media.Image;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.transition.Slide;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class SliderAdapter extends PagerAdapter {
+
+    public int[] slide_images = {
+            R.drawable.eat_icon,
+            R.drawable.sleep_icon,
+            R.drawable.code_icon
+    };
+    public String[] slide_heading = {
+            "EAT", "SLEEP", "CODE"
+    };
+    public String[] slide_desc =
+            {
+                    "loream isprihdsbfhd fdnfjnd fdfndjs nfodsjn",
+                    "loream isprihdsbfhd fdnfjnd fdfndjs nfodsjn",
+                    "loream isprihdsbfhd fdnfjnd fdfndjs nfodsjn"
+            };
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public SliderAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return slide_heading.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view == (RelativeLayout) o;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_layout, container, false);
+
+        ImageView slideImageView = (ImageView) view.findViewById(R.id.slide_image);
+        TextView slideHeading = (TextView) view.findViewById(R.id.slide_heading);
+        TextView slideDescription = (TextView) view.findViewById(R.id.slide_desc);
+
+        slideImageView.setImageResource(slide_images[position]);
+        slideHeading.setText(slide_heading[position]);
+        slideDescription.setText(slide_desc[position]);
+
+        container.addView(view);
+
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((RelativeLayout) object);
+    }
+}
